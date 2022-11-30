22e6e8198e48932aade5307278d5e87856a6dd49
Kasper Nikolajsen Dahl <kasp103y@stud.kea.dk>
2022-11-30 17:57:38 +0100

    udstillinger component begun

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const burgerIcon = document.querySelector(".burger-icon");
 const burgerMenu = document.querySelector(".burger");
+/* const urlUdstilligner = "images.json"; */
+const udstilImg = document.querySelector(".udstilling-link");
 
 burgerIcon.addEventListener("click", burgerOnClick);
+
+/* getUdstillinger(); */
 
 function burgerOnClick() {
   burgerMenu.classList.toggle("open");
   burgerIcon.classList.toggle("close-icon");
+}
+
+async function getUdstillinger() {
+  const response = await fetch(urlUdstilligner);
+  const udstillinger = await response.json();
+  console.log(udstillinger);
+  visUdstillinger(udstillinger);
+}
+
+function visUdstillinger(udstillinger) {
+  udstillinger.forEach((udstilling) => {
+    console.log(udstilling.Image);
+  });
 }
